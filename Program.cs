d5adf23c2b97bdb9668ab1cf02cb00cf03cc00ad
Константин Силенков <silenkov@inbox.ru>
2024-01-27 14:26:03 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace MonitoringTask
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var systeminfo = new SystemInfo();
+            string cpuCounter = String.Empty;
+            string ramCounter = String.Empty;
+
+            var threadGetCpu = new Thread(() =>
+            {
+                while(true)
+                {
+                    cpuCounter = systeminfo.GetCpu();
+                    Thread.Sleep(1000);
+                }
+            });
+
+            var threadGetRam = new Thread(() =>
+            {
+                while(true)
+                {
+                    ramCounter = systeminfo.GetRam();
+                    Thread.Sleep(500);
+                }
+            });
+
+            threadGetCpu.Start();
+            threadGetRam.Start();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(cpuCounter);
+                Console.WriteLine(ramCounter);
+                Thread.Sleep(800);
+            }
+        }
+    }
+}
---
+++ SystemInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace MonitoringTask
+{
+    /// <summary>
+    /// Позволяет получать информацию о свободной памяти RAM и загрузке процессора
+    /// </summary>
+    public class SystemInfo
+    {
+        private PerformanceCounter _cpuCounter = new("Processor", "% Processor Time", "_Total");
+        private PerformanceCounter _ramCouner = new("Memory", "Available MBytes");
+
+        public string GetCpu()
+        {
+            return _cpuCounter.NextValue() + " %";
+        }
+
+        public string GetRam()
+        {
+            return _ramCouner.NextValue() + " MB";
+        }
+    }
+}
